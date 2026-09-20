9332bcb7ff3f89dd56952d8a7c3b293d0b8acafb
Hamayun Javed <hamayunjaved007@gmail.com>
2026-09-20 13:21:11 +0500

    Registeration form cretaed

---
+++ actions/register.php
@@ -0,0 +1,13 @@
+<?php
+$name = $_POST['name'];
+$email = $_POST['email'];
+$whatsapp = $_POST['whatsapp'];
+$password = $_POST['password'];
+$confirm_password = $_POST['confirm_password'];
+$gender = $_POST['gender'];
+
+echo $name . ' ' . $email . ' ' . $whatsapp . ' ' . $password . ' ' . $confirm_password . ' ' . $gender;
+// if($password == $confirm_password){
+
+// }
+?>
--- register.php
+++ register.php
@@ -1,5 +1,168 @@
 <?php include('includes/header.php'); // Header ?>
 
-<h2>Register Page</h2>
+
+<div class="container my-5">
+
+    <div class="row justify-content-center">
+
+        <div class="col-12 col-sm-10 col-md-6 col-lg-4">
+
+            <div class="card shadow border-0">
+
+                <!-- Header -->
+                <div class="card-header text-center text-white" style="background-color: #01411C;">
+
+                    <h3 class="mb-1">
+                        <i class="fa fa-user-plus"></i>
+                        Register
+                    </h3>
+
+                </div>
+
+
+                <!-- Registration Form -->
+                <div class="card-body p-4">
+
+                    <form action="actions/register.php" method="post">
+
+                        <!-- Name -->
+                        <div class="mb-3">
+
+                            <label class="form-label">
+                                <i class="fa fa-user"></i>
+                                Name
+                            </label>
+
+                            <input type="text" class="form-control" placeholder="Enter your name" name="name" required>
+
+                        </div>
+
+
+                        <!-- Email -->
+                        <div class="mb-3">
+
+                            <label class="form-label">
+                                <i class="fa fa-envelope"></i>
+                                Email
+                            </label>
+
+                            <input type="email" class="form-control" placeholder="Enter your email" name="email"
+                                required>
+
+                        </div>
+
+
+                        <!-- WhatsApp Number -->
+                        <div class="mb-3">
+
+                            <label class="form-label">
+                                <i class="fa fa-whatsapp"></i>
+                                Enter WhatsApp Number
+                            </label>
+
+                            <input type="tel" class="form-control" placeholder="e.g. 0300 1234567" name="whatsapp"
+                                required maxlength="11">
+
+                        </div>
+
+
+                        <!-- Password -->
+                        <div class="mb-3">
+
+                            <label class="form-label">
+                                <i class="fa fa-lock"></i>
+                                Password
+                            </label>
+
+                            <input type="password" class="form-control" placeholder="Enter your password"
+                                name="password" required>
+
+                        </div>
+
+
+                        <!-- Confirm Password -->
+                        <div class="mb-3">
+
+                            <label class="form-label">
+                                <i class="fa fa-lock"></i>
+                                Confirm Password
+                            </label>
+
+                            <input type="password" class="form-control" placeholder="Confirm your password"
+                                name="confirm_password" required>
+
+                        </div>
+
+
+                        <!-- Gender -->
+                        <div class="mb-3">
+
+                            <label for="" class="form-label d-block">
+                                <i class="fa fa-users"></i>
+                                Gender
+                            </label>
+
+                            <div class="form-check form-check-inline">
+
+                                <input class="form-check-input" type="radio" name="gender" id="male" value="male"
+                                    required>
+
+                                <label class="form-check-label" for="male">
+                                    <i class="fa fa-male"></i>
+                                    Male
+                                </label>
+
+                            </div>
+
+
+                            <div class="form-check form-check-inline">
+
+                                <input class="form-check-input" type="radio" name="gender" id="female" value="female">
+
+                                <label class="form-check-label" for="female">
+                                    <i class="fa fa-female"></i>
+                                    Female
+                                </label>
+
+                            </div>
+
+                        </div>
+
+
+                        <!-- Register Button -->
+                        <div class="d-grid mt-4">
+
+                            <button type="submit" class="btn btn-success">
+                                <i class="fa fa-user-plus"></i>
+                                Register
+                            </button>
+
+                        </div>
+
+                    </form>
+
+
+                    <!-- Login Link -->
+                    <div class="text-center mt-4">
+
+                        <span>Already have an account?</span>
+
+                        <a href="#" class="text-decoration-none fw-bold">
+                            <i class="fa fa-sign-in"></i>
+                            Login
+                        </a>
+
+                    </div>
+
+                </div>
+
+            </div>
+
+        </div>
+
+    </div>
+
+</div>
+
 
 <?php include('includes/footer.php'); // Footer ?>
